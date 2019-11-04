31cd821754a809764057080dc68cabe934f7944c
tts <thomas871208@gmail.com>
2019-11-04 21:39:18 +0800

    2nd

--- app/src/main/java/com/tts/foodmemutest/MainActivity.java
+++ app/src/main/java/com/tts/foodmemutest/MainActivity.java
@@ -1,18 +1,36 @@
 package com.tts.foodmemutest;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ed_h;
+    EditText ed_f;
+    EditText ed_hot;
+    EditText ed_d;
+    ImageView x1;
+    ImageView x2;
+    ImageView x3;
+    ImageView x4;
+    int ham;
+    int fries;
+    int hotdog;
+    int drink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ed_h = findViewById(R.id.ed_h);
+        ed_f = findViewById(R.id.ed_f);
+        ed_hot = findViewById(R.id.ed_hot);
+        ed_d = findViewById(R.id.ed_d);
+
+        x1 = findViewById(R.id.x1);
+        x2 = findViewById(R.id.x2);
+        x3 = findViewById(R.id.x3);
+        x4 = findViewById(R.id.x4);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +65,57 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public  void order(View view){
+        ham = ed_h.getText().toString().length();
+        fries = ed_f.getText().toString().length();
+        hotdog = ed_hot.getText().toString().length();
+        drink = ed_d.getText().toString().length();
+        if(ham == 0 || fries ==0 || hotdog ==0 || drink == 0){
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Error")
+                    .setMessage("cannot be empty, please enter a number")
+                    .setPositiveButton("OK",null)
+                    .show();
+            if(ham == 0){
+                x1.setImageResource(R.drawable.x);
+                x1.setVisibility(View.VISIBLE);
+            }else {
+                x1.setVisibility(View.GONE);
+            }
+            if(fries == 0){
+                x2.setImageResource(R.drawable.x);
+                x2.setVisibility(View.VISIBLE);
+            }else {
+            x2.setVisibility(View.GONE);
+        }
+            if(hotdog == 0){
+                x3.setImageResource(R.drawable.x);
+                x3.setVisibility(View.VISIBLE);
+            }else {
+                x3.setVisibility(View.GONE);
+            }
+            if(drink == 0){
+                x4.setImageResource(R.drawable.x);
+                x4.setVisibility(View.VISIBLE);
+            }else {
+                x4.setVisibility(View.GONE);
+            }
+
+        }else{
+            x1.setVisibility(View.GONE);
+            x2.setVisibility(View.GONE);
+            x3.setVisibility(View.GONE);
+            x4.setVisibility(View.GONE);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Tip")
+                    .setMessage("thank you for your order")
+                    .setPositiveButton("OK",null)
+                    .show();
+
+        }
+
     }
 
     @Override
